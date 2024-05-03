b5653c616cc833f46d97598ea26b45da8474a82f
fernandog301 <fernandog301g@gmail.com>
2024-05-03 12:47:30 -0700

    test

--- src/app/CreateAccount/page.tsx
+++ src/app/CreateAccount/page.tsx
@@ -1,36 +1,36 @@
-"use client"
+// "use client"
 
-import AddLocationComponent from '@/Components/CreateAccount/AddLocation/page'
-import AddMenuItemsComponent from '@/Components/CreateAccount/AddMenuItems/page'
-import { SignUpProvider } from '@/context/Context'
-import React, { useState } from 'react'
+// import AddLocationComponent from '@/Components/CreateAccount/AddLocation/page'
+// import AddMenuItemsComponent from '@/Components/CreateAccount/AddMenuItems/page'
+// import { SignUpProvider } from '@/context/Context'
+// import React, { useState } from 'react'
 
-const CreateAccount = () => {
+// const CreateAccount = () => {
 
-  const componentArray = [<AddMenuItemsComponent/>, <AddLocationComponent />, ]
-  const [pageArr, setPageArr] = useState(0);
-  const ChangeComponent = () => {
-    return componentArray[pageArr]
-  }
+//   // const componentArray = [<AddMenuItemsComponent/>, <AddLocationComponent />, ]
+//   // const [pageArr, setPageArr] = useState(0);
+//   // const ChangeComponent = () => {
+//   //   return componentArray[pageArr]
+//   }
 
-  return (
-    <SignUpProvider>
-      <div className='flex justify-center items-center h-full'>
-            <div className="px-12 py-12 bg-white rounded-3xl flex flex-col">
-                <div>
-                    <h1 className=' text-rose-600 text-center text-4xl mt-4'>Prometo Food</h1>
-                    <h1 className=' text-rose-600 text-center text-4xl'>Trucks</h1>
-                </div>
+//   return (
+//     <SignUpProvider>
+//       <div className='flex justify-center items-center h-full'>
+//             <div className="px-12 py-12 bg-white rounded-3xl flex flex-col">
+//                 <div>
+//                     <h1 className=' text-rose-600 text-center text-4xl mt-4'>Prometo Food</h1>
+//                     <h1 className=' text-rose-600 text-center text-4xl'>Trucks</h1>
+//                 </div>
             
-        {ChangeComponent()}
-        <div>
-        <button onClick={() => setPageArr(pageArr + 1)}> WE GO NEXT </button>
-        </div>
-        </div>
-      </div>
-    </SignUpProvider>
+//         {ChangeComponent()}
+//         <div>
+//         <button onClick={() => setPageArr(pageArr + 1)}> WE GO NEXT </button>
+//         </div>
+//         </div>
+//       </div>
+//     </SignUpProvider>
     
-  )
-}
+//   )
+// }
 
-export default CreateAccount
+// export default CreateAccount
